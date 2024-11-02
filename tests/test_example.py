26cd47aee47ab5379186acc1dabec194ce920621
Marvv1ne <gorn89@inbox.ru>
2024-11-02 09:36:11 +0400

    add fixture and test with fixture

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,3 +6,12 @@ def test_reverse():
 def test_reverse_for_empty_string():
     assert reverse('') == ''
 
+def test_reverse_with_fixture():
+    fixture_path = 'tests/fixtures/before.txt'
+    correct_answer_path = 'tests/fixtures/after.txt'
+    with open(fixture_path, encoding='utf8') as f:
+        with open(correct_answer_path, encoding='utf8') as a:
+            string = f.readline().strip()
+            reversed_string = a.readline().strip()
+            assert reverse(string) == reversed_string
+
